644f26dc12c9dc2da1a68485f4b359cb49976afe
MrMoss12-cmd <eliashigueraacosta1@outlook.com>
2024-12-19 10:38:49 -0500

    first commit

---
+++ tech_track/equipment/models.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_address
+from django.db import models
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=255)
+    ip_address = models.GenericIPAddressField()
+    status = models.CharField(max_length=50, default='offline')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
---
+++ tech_track/equipment/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Equipment
+from .serializers import EquipmentSerializer
+
+class EquipmentViewSet(ModelViewSet):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
